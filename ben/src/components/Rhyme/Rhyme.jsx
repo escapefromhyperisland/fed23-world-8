a3688f6fd130986c09b99f116b8f419a9242ed65
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-31 11:30:03 +0200

    User input returns rhyming words

--- ben/src/components/Rhyme/Rhyme.jsx
+++ ben/src/components/Rhyme/Rhyme.jsx
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { checkRhymeFromAPI } from "../../scripts/api"
 
 const Rhyme = () => {
-  let word = "happy";
-  
+  const [word, setWord] = useState("");
+
+  function updateWord(e) {
+    e.preventDefault();
+    setWord(e.target[0].value);
+    console.log(word);
+  }
 
   useEffect(() => {
     checkRhymeFromAPI(word);
+    console.log("Word was updated");
   }, [word]);
 
     return(
-      <h1>Rhyme test</h1>
+      <section>
+        <h1>Rhyme Test</h1>
+        <form onSubmit={updateWord}>
+          <input type="text" id="word" name="word" />
+        </form>
+      </section>
     )
 }
 
