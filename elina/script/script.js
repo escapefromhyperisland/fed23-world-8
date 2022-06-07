22c062e7abebc8853badf76d530497f04c6cf6d7
ElinaHulbert <elina.hulbert@hyperisland.se>
2022-06-07 12:32:53 +0200

    first floor set up

---
+++ elina/script/script.js
@@ -0,0 +1,6 @@
+// console.log("try");
+// let roots = document.querySelector('#roots');
+// console.log(roots);
+// roots.addEventListener('click', (event) =>  {
+//     console.log('Entity collided with');
+//   });
