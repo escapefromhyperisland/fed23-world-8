ae3d00652078302e8b4625f62b76458cb0ad48d5
koltrastH <elina.hulbert@hyperisland.se>
2022-06-08 16:05:28 +0200

    fixed eventListener

--- elina/script/script.js
+++ elina/script/script.js
@@ -4,3 +4,30 @@
 // roots.addEventListener('click', (event) =>  {
 //     console.log('Entity collided with');
 //   });
+
+{
+  /* <html>
+  <head>
+    <script src="https://unpkg.com/aframe@latest"></script>
+    <script>
+      AFRAME.registerComponent("click-listener", {
+        init: function () {
+          const url = this.data.url;
+          this.el.addEventListener("click", function (evt) {
+            alert("Clicked!");
+          });
+        },
+      });
+    </script>
+  </head>
+  <body>
+    <a-scene cursor="rayOrigin: mouse">
+      <a-box
+        click-listener
+        position="0 1.6 -4"
+        color="#4CC3D9"
+      ></a-box>
+    </a-scene>
+  </body>
+</html> */
+}
